11a8851632525030c0082a28688b1b09a949f525
Svdbroek <s_in_the_water@hotmail.com>
2019-11-07 17:30:26 +0100

    button shows answer, no refresh

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const questionList = [
     [['What is an Object?'],['a collection var used to store any kind of var behind a key']],
     [['seven times seven =?'],['49']]
 ]
+let questionPair =Math.floor((Math.random() * questionList.length))
 
-let question=questionList[Math.floor((Math.random() * questionList.length))][0]
+let question=questionList[questionPair][0]
+let answer=questionList[questionPair][1]
 
 document.getElementById('question').innerHTML=question
+
+let showAnswer=()=>{
+    document.getElementById('answer').innerHTML=answer
+}
