44a6b5b483e53666db9281613c98edf4f5f1f9dd
Svdbroek <s_in_the_water@hotmail.com>
2019-11-07 17:41:50 +0100

    button gives new question without being refreshed

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const questionList = [
-    [['What is an Array?'],['a collection variable used to store any kind of var in a list']],
-    [['What is an Object?'],['a collection var used to store any kind of var behind a key']],
-    [['seven times seven =?'],['49']]
+    [['What is an Array?'], ['a collection variable used to store any kind of var in a list']],
+    [['What is an Object?'], ['a collection var used to store any kind of var behind a key']],
+    [['seven times seven =?'], ['49']]
 ]
-let questionPair =Math.floor((Math.random() * questionList.length))
+answer = ''
 
-let question=questionList[questionPair][0]
-let answer=questionList[questionPair][1]
 
-document.getElementById('question').innerHTML=question
-
-let showAnswer=()=>{
-    document.getElementById('answer').innerHTML=answer
+let showAnswer = () => {
+    document.getElementById('answer').innerHTML = answer
 }
+
+let newQuestion = () => {
+    let questionPair = Math.floor((Math.random() * questionList.length))
+
+    let question = questionList[questionPair][0]
+     answer = questionList[questionPair][1]
+
+    document.getElementById('question').innerHTML = question
+    document.getElementById('answer').innerHTML = ''
+}
+
+newQuestion();
